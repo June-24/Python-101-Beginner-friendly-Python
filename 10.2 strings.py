526f6c9387083e161b8203dd7a01882754a42d20
June-24 <mj21csb0b36@student.nitw.ac.in>
2023-03-07 22:54:46 +0530

    yes

--- 10.2 strings.py
+++ 10.2 strings.py
@@ -1,4 +1,8 @@
 sentence = """A sentence in here
 
 And on a new line"""
+#string.upper()->capitals
+#""" in 3 quotes multi lines"""
+#string. just enter this and you can see all the function
+#type(variable)->gives the class tyoe
 print(sentence.upper())
--- 9.2 integers.py
+++ 9.2 integers.py
@@ -6,7 +6,9 @@ print(total)
 #suppose
 price="19.97"
 #it does the printing 4 times not any error
+#very important
 total = items*price
 print(total)
-
+#no need to put 0s all in a line, to make it readible 
+#just use underscore
 print(1_000_000_000_000)
