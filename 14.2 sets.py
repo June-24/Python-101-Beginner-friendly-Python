073c3a135d8ea313071fbe1c627bd65d5fbb544f
June-24 <mj21csb0b36@student.nitw.ac.in>
2023-03-07 23:05:23 +0530

    Update 14.2 sets.py

--- 14.2 sets.py
+++ 14.2 sets.py
@@ -2,5 +2,13 @@
 # dictionary = {
 #     "key": "value",
 # }
+#looks similar to dictionary and a list 
+#set is given below
+#order is not important, all random
 s = {"Item 1", "Item 2", "Item 2", "Item 3"}
+s
 print(s)
+#see repeated elemnts are not shown
+#order also different
+for item in s:
+    print("item in set is : ",item)
